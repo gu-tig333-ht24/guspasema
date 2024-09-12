d31a2616c27cf3a117bca18662815fc0fdc1c6d7
emilpasse <emil@passe.se>
2024-09-12 17:42:54 +0200

    List and views

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:ffi';
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -16,41 +18,71 @@ class MyApp extends StatelessWidget {
   }
 }
 
+class Task {
+  String taskName;
+  bool isComplete;
+
+  Task(this.taskName, this.isComplete); //constructor för task
+}
+
 class MyHomePage extends StatelessWidget {
   const MyHomePage({super.key});
 
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    //skapar en lista med tasks och väljer deras defualtvärden
+    List<Task> tasks = [
+      Task("Do HomeWork", false),
+      Task("Write a book", false),
+    ];
     return Scaffold(
-        appBar: AppBar(title: const Text("TIG333 TODO")),
-        body: Column(children: [
-          _item("Write a book"),
-          _item("Do homework"),
-        ]));
+      appBar: AppBar(title: const Text("TIG333 TODO")),
+      body: ListView(
+        children: tasks
+            .map((task) => _item(context, task.taskName, task.isComplete))
+            .toList(), //map är en iterator som returerar en lista
+      ),
+    );
   }
 
-  Widget _item(String task) {
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.center,
-      crossAxisAlignment: CrossAxisAlignment.center,
-      children: [
-        Padding(
-          padding: EdgeInsets.fromLTRB(15, 10, 15, 10),
-          child: Checkbox(value: false, onChanged: (bool? value) {}),
-        ),
-        Expanded(
-          child: Text(
-            task,
-            style: TextStyle(fontSize: 24),
-            textAlign: TextAlign.left,
+  Widget _item(BuildContext context, task, bool isComplete) {
+    return GestureDetector(
+      onTap: () {
+        //när du klickar på raden så kommer du navigeras till TaskInfo vyn
+        Navigator.push(
+            context, MaterialPageRoute(builder: (context) => TaskInfo()));
+      },
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Padding(
+            padding: EdgeInsets.fromLTRB(15, 10, 15, 10),
+            child: Checkbox(value: false, onChanged: (bool? value) {}),
           ),
-        ),
-        Padding(
-          padding: EdgeInsets.all(15),
-          child: Icon(Icons.close),
-        )
-      ],
+          Expanded(
+            child: Text(
+              task,
+              style: TextStyle(fontSize: 24),
+              textAlign: TextAlign.left,
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.all(15),
+            child: Icon(Icons.close),
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class TaskInfo extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
     );
   }
 }
