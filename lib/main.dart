00d83bc3c2f0c573c10e53957309f3b951f4c116
emilpasse <emil@passe.se>
2024-09-13 15:18:53 +0200

    steg1 complete

--- lib/main.dart
+++ lib/main.dart
@@ -47,24 +47,58 @@ class MyHomePage extends StatelessWidget {
       Task("Write a book", false),
     ];
     return Scaffold(
-      appBar: AppBar(title: const Text("TIG333 TODO")),
+      appBar: AppBar(
+        centerTitle: true,
+        title: const Text(
+          "TIG333 TODO",
+          style: TextStyle(fontSize: 30),
+        ),
+        backgroundColor: const Color.fromARGB(255, 224, 224, 224),
+        scrolledUnderElevation: 0,
+        actions: [
+          PopupMenuButton<int>(
+            color: (Colors.white),
+            itemBuilder: (context) => [
+              PopupMenuItem(
+                child: Text("all"),
+              ),
+              PopupMenuItem(
+                child: Text("done"),
+              ),
+              PopupMenuItem(
+                child: Text("undone"),
+              ),
+            ],
+          ),
+        ],
+      ),
       body: ListView(
         children: tasks
             .map((task) => _item(context, task.taskName, task.isComplete))
             .toList(), //map är en iterator som returerar en lista
       ),
-
       //när du klickar på knappen så kommer du navigeras till AddTask vyn
       //den gör detta via en stack, och vyn pushas till toppen av stacken med funktionen push
-      floatingActionButton: FloatingActionButton(onPressed: () {
-        Navigator.push(
-            context, MaterialPageRoute(builder: (context) => AddTask()));
-      }),
+
+      floatingActionButton: RawMaterialButton(
+        onPressed: () {
+          Navigator.push(
+              context, MaterialPageRoute(builder: (context) => AddTask()));
+        },
+        fillColor: const Color.fromARGB(255, 224, 224, 224),
+        shape: CircleBorder(),
+        child: Icon(
+          Icons.add,
+          size: 75,
+          color: Colors.white,
+        ),
+      ),
     );
   }
 
   Widget _item(BuildContext context, task, bool isComplete) {
     return GestureDetector(
+      //gesture detect används eftersom det inte är en knapp utan bara en lista med items
       onTap: () {},
       child: Row(
         mainAxisAlignment: MainAxisAlignment.center,
@@ -91,34 +125,54 @@ class MyHomePage extends StatelessWidget {
   }
 }
 
+//add task vyn, man kommer hit genom att trycka pluset i homepage vyn
 class AddTask extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
+      appBar: AppBar(
+        centerTitle: true,
+        title: const Text(
+          "TIG333 TODO",
+          style: TextStyle(fontSize: 30),
+        ),
+        backgroundColor: const Color.fromARGB(255, 224, 224, 224),
+        scrolledUnderElevation: 0,
+      ),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.start,
           crossAxisAlignment: CrossAxisAlignment.center,
           children: [
             Padding(
-              padding: EdgeInsets.fromLTRB(15, 10, 15, 10),
+              padding: EdgeInsets.fromLTRB(15, 25, 15, 35),
               child: TextField(
                 decoration: InputDecoration(
+                    //dekorationer för textfältet
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(4)),
+                      borderSide: BorderSide(width: 2, color: Colors.black),
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderSide: BorderSide(width: 2, color: Colors.black),
+                    ),
                     hintStyle: TextStyle(color: Colors.black.withOpacity(0.3)),
                     hintText: "What are you going to do?"),
               ),
             ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                IconButton(
-                  icon: Icon(Icons.add),
-                  onPressed: () {},
-                ),
-                Text("Add"),
-              ],
+            FloatingActionButton.extended(
+              //extended gör att både en icon och text kan vara i knappen
+              elevation: 0,
+              label: Text("ADD",
+                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20)),
+              icon: Icon(
+                Icons.add,
+                size: 25,
+              ),
+              backgroundColor: Colors.transparent,
+              foregroundColor: Colors.black,
+              splashColor: const Color.fromARGB(255, 202, 202, 202),
+              onPressed: () {}, //knappen gör inget än
             )
           ],
         ),
